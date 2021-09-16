6552e07ab69d16b254af5a99454e682dcacdbbdc
Minchao Duclos <chaohg@hotmail.com>
2021-09-16 12:22:47 -0700

    add search form

--- src/components/Space.js
+++ src/components/Space.js
@@ -1,11 +1,14 @@
 import React, {useEffect, useState} from 'react';
-import {Card, CardImg, CardTitle, CardBody} from 'reactstrap'
-
-const url = 'https://api.nasa.gov/planetary/apod?start_date=2015-09-07&end_date=2015-09-15&api_key=ORrPe6BufYrJ07FVPGoheu9wjNmuvrarg4SVKlhN'
 
 function Space() {
     const [loading, setLoading] = useState(true)
     const [data, setData] = useState([])
+    const [startDate, setStartdate] = useState('')
+    const [endDate, setEnddate] = useState('')
+
+    const url = 'https://api.nasa.gov/planetary/apod?start_date=2015-09-07&end_date=2015-09-15&api_key=ORrPe6BufYrJ07FVPGoheu9wjNmuvrarg4SVKlhN'
+
+    const searchUrl = `https://api.nasa.gov/planetary/apod?start_date=${startDate}&end_date=${endDate}&api_key=ORrPe6BufYrJ07FVPGoheu9wjNmuvrarg4SVKlhN`
 
     const fetchSpace = async() => {
         setLoading(true)
@@ -22,28 +25,76 @@ function Space() {
         }
     }
 
+    const SearchSpace = async() => {
+        setLoading(true)
+        try {
+            const response = await fetch(searchUrl);
+            const searchspace = await response.json();
+            setLoading(false);
+            setData(searchspace)
+
+        } catch(error) {
+            setLoading(false);
+            throw error
+
+        }
+    }
+
+    const handleInputChange = (e) => {
+        setEnddate(e.target.endDate);
+        setStartdate(e.target.startDate)
+    }
+
     useEffect(() => {
         fetchSpace();
     },[])
 
-    console.log(data)
+    // useEffect(() => {
+    //     SearchSpace()
+    // },[startDate])
 
     return (
-        data.map(item => {
-            return (
-                <div className = 'container'>
-                    <Card>
-                        <CardImg height='300rem' src={item.hdurl} />
-                        <CardBody>
-                            <CardTitle>
-                                {item.caption}
-                            </CardTitle>
-                        </CardBody>
-                    
-                    </Card>
-                </div>
-            )
-        })
+        <div className = "section-center ">
+            <h1> Spacestagram </h1>
+            <div  className= 'row' style={{fontSize: '2rem'}} >
+                <label className = 'pb-3' >
+                    Start Date:
+                    <input type='date' name='startdate'
+                            onChange = {handleInputChange}
+                            style={{margin:'1rem', fontSize:'1.5rem'}}/>
+                </label>
+                <label>
+                    End Date:
+                    <input type='date' name='enddate'
+                            onChange = {handleInputChange}
+                            style={{margin:'1rem', fontSize:'1.5rem'}} />
+                </label>
+            </div>
+            <div className = 'row'>
+               
+                <button type='submit' 
+                        onClick = {SearchSpace}
+                        style={{fontSize: '4vh', color: 'white', backgroundColor: 'green', padding: '1rem', borderWidth:'0.2rem', borderColor:'black',borderRadius:'1rem'}} >
+                        Search
+                </button>
+            </div>
+
+                {data.map(item => {
+                    return (
+                        
+                        <article className='space-item'>
+                            <img src = {item.url} alt = {item.image} className = 'photo' />
+                            <div className = 'space-info'>
+                            <header>
+                                <h4>{item.title}</h4>
+                            </header>
+                            <p className = 'space-text'>{item.explanation}</p>
+                            </div>
+                    </article>
+                    )
+                })}
+            </div>
+      
     )
 }
 
